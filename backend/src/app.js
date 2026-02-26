903617c300e58082f9cdd5dbef622ce8784457d4
LiClement0216 <clementli0216@gmail.com>
2026-02-27 07:54:16 +0800

    routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,27 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(htmlDir, 'index.html'));
 });
 
+app.get('/cakes/cake:idx', (req, res) => {
+  const idx = req.params.idx;             
+  const file = `cake${idx}.html`;
+  res.sendFile(path.join(htmlDir, 'cakes', file));
+});
+
+app.get('/categories/category:idx', (req, res) => {
+  const idx = req.params.idx;             
+  const file = `category${idx}.html`;
+  res.sendFile(path.join(htmlDir, 'categories', file));
+});
+
+app.get('/macarons/macaron:idx', (req, res) => {
+  const idx = req.params.idx;             
+  const file = `macaron${idx}.html`;
+  res.sendFile(path.join(htmlDir, 'macarons', file));
+});
+
+
+
+
 const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
