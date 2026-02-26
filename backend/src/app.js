12d8793680161bae361573b45346136e791bd2c3
LiClement0216 <clementli0216@gmail.com>
2026-02-27 07:40:32 +0800

    dir correct

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,15 @@ const express = require('express');
 
 const app = express();
 
-const htmlDir = path.join(__dirname, '..', '..', 'frontend', 'statics', 'html');
-const staticDir = path.join(__dirname, '..', '..', 'frontend');
-
+const staticDir = path.join(__dirname, '..', '..', 'frontend', 'statics');
 app.use(express.static(staticDir));
 
+const htmlDir = path.join(staticDir, 'html');
 app.get('/', (req, res) => {
   res.sendFile(path.join(htmlDir, 'index.html'));
 });
 
 const PORT = 3000;
-app.listen(PORT,'0.0.0.0',() => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
